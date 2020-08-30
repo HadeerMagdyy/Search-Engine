f65db060c4c4434324175302e0209fc428693878
HadeerMagdyy <hadeermg4@gmail.com>
2020-08-31 01:40:04 +0200

    First commit

---
+++ Search Engine/Controllers/databaseContext.cs
@@ -0,0 +1,7 @@
+﻿namespace Search_Engine.Controllers
+{
+    internal class databaseContext
+    {
+        public object Inverted_index { get; internal set; }
+    }
+}
---
+++ Search Engine/Models/searchquery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Search_Engine.Models
+{
+    public class searchquery
+    {
+        
+        public string query { get; set; } //variable to hold user query which entered in search space , this variable has been filled in searchquery() in search controller
+
+
+
+        //Function to apply tokenization and case folding , split query into words and store those words in 'searchquery_terms'
+
+        public string[] Query_to_words()
+        {
+            char[] split_token = { ' ', ',', '!', '@', '#', '&', '“', '”', '‘', '—', '(', ')', '–', '[', '{', '}', ']', ':', ';', '?', '/', '*', '.', '-', '"', '\'', '\\', '%', '$', '^', '\r', '\n', '~', '`', '’', ',', '?', '_' };
+            string searchquery = query.ToLower();
+            string[] searchquery_terms = searchquery.Split(split_token);
+            return searchquery_terms;
+        }
+    }
+}
